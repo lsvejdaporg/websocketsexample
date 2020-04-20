5fc04c7e451db46f94580ef14f76d74f44d5a829
Libor Svejda <libor.svejda@gmail.com>
2020-04-20 11:12:10 +0200

    Procisteni od ukazky pouziti MySQL databaze.

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const createSpaServer = require("spaserver").createSpaServer;
 const apiDenVTydnu = require('./api-denvtydnu').apiDenVTydnu;
 const apiCas = require('./api-cas').apiCas;
-const apiDb = require('./api-db').apiDb;
-const apiDbGen = require('./db-setup/api-db-gen').apiDbGen;
 
 const PORT = 8080; //aplikace na Rosti.cz musi bezet na portu 8080
 const API_HEAD = {
@@ -17,18 +15,10 @@ function processApi(req, res) {
     let obj = {};
     obj.status = API_STATUS_OK;
 
-    if (req.pathname === "/dbgen") { //v ostre aplikaci nebude toto treba
-        apiDbGen(req, res, obj);
-        return;
-    }
-
     if (req.pathname === "/denvtydnu") {
         apiDenVTydnu(req, res, obj);
     } else if (req.pathname === "/cas") {
         apiCas(req, res, obj);
-    } else if (req.pathname.startsWith("/db")) {
-        apiDb(req, res, obj);
-        return; //MySQL query je asynchronni
     } else {
         obj.status = API_STATUS_NOT_FOUND;
         obj.error = "API not found";
